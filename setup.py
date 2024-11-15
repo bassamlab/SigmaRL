b6d944c727a137c5c54a43d7f56ea544096c33ae
Jianye Xu <jianye.xu@rwth-aachen.de>
2024-11-15 12:35:53 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -14,41 +14,40 @@ requirements = (HERE / "requirements.txt").read_text().splitlines()
 
 
 setup(
-    name="sigmarl",  # Required
-    version="1.2.0",  # Required
-    author="Jianye Xu",  # Replace with your name
-    author_email="xu@embedded.rwth-aachen.de",  # Replace with your email
-    description="SigmaRL: A Sample-Efficient and Generalizable Multi-Agent Reinforcement Learning Framework for Motion Planning",  # Replace with a short description
-    long_description=README,  # Long description read from README.md
+    name="sigmarl",
+    version="1.2.0",
+    author="Jianye Xu",
+    author_email="xu@embedded.rwth-aachen.de",
+    description="SigmaRL: A Sample-Efficient and Generalizable Multi-Agent Reinforcement Learning Framework for Motion Planning",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/cas-lab-munich/SigmaRL",  # Replace with your repo URL
-    packages=find_packages(),  # Automatically find packages in the directory
-    classifiers=[  # Optional classifiers
+    url="https://github.com/bassamlab/SigmaRL",
+    packages=find_packages(),
+    classifiers=[
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "License :: MIT License",  # Change if you use a different license
+        "License :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9, <3.11",  # Specify your Python version
+    python_requires=">=3.9, <3.10",
     install_requires=[
         "gym==0.26.2",
-        "numpy",
         "tensordict==0.2.1",
         "torch==2.1.0",
         "torchrl==0.2.1",
         "tqdm==4.66.1",
         "typing_extensions==4.8.0",
         "vmas==1.4.1",
-        "matplotlib",
         "termcolor==2.4.0",
-        "SciencePlots==2.1.1",
         "pre-commit==3.7.1",
+        "matplotlib",
+        "numpy",
     ],
-    extras_require={  # Optional
-        "dev": ["pytest", "sphinx"],  # Development dependencies
+    extras_require={
+        "dev": ["pytest", "sphinx"],
     },
-    include_package_data=True,  # Include non-Python files specified in MANIFEST.in
-    package_data={  # Include additional files within packages
-        "sigmarl": ["assets/*", "scenarios/*", "config.json"],  # Adjust as needed
+    include_package_data=True,
+    package_data={
+        "sigmarl": ["assets/*", "scenarios/*", "config.json"],
     },
 )
